cb2137c5da9a98e01695fe60231576f53564d42a
Daniel Tan <dtan004@hammer.cs.ucr.edu>
2017-10-28 10:28:55 -0700

    Added the initial code to the main file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
-#include <unistd.h>
 #include <stdio.h>
 
 int main() {
-
-
+	FILE *f = popen("ls", "r");
+	char buffer[1024];
+	
+	if(!f) {
+		printf("There were errors");
+		return -1;
+	}
+	//while(fgets(buffer, sizeof(buffer), f)) {
+	//	printf("%s", buffer);
+	//}
+	
+	pclose(f);
 }
