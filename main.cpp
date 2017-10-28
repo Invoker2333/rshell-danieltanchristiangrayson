15bd1983d786988c4d294d60bdc47cf9b6cea6ba
Daniel Tan <dtan004@hammer.cs.ucr.edu>
2017-10-28 10:56:38 -0700

    main.cpp can now execute shell commands

--- main.cpp
+++ main.cpp
@@ -1,16 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+
+int executeCommands(const char *const NAME) {
+	FILE *file = popen(NAME, "r");
+	char buffer[1024];
+
+	if(file == 0)
+		return 0;
+
+	while(fgets(buffer, sizeof(buffer), file)) {
+		printf("%s", buffer);
+	}
+	
+	pclose(file);
+	return 1;
+}
 
 int main() {
-	FILE *f = popen("ls", "r");
-	char buffer[1024];
+	char buffer[2048];
+	while(1) {
+		
+		printf("Type in a command to execute: ");
+		scanf("%s", buffer);
+
+		if(strcmp(buffer, "exit") == 0) {
+			printf("END\n");
+			return 0;
+		}
 	
-	if(!f) {
-		printf("There were errors");
-		return -1;
+		if(executeCommands(buffer)) {
+			printf("Commands successfully executed.\n");
+		} else {
+			printf("Commands failed\n");
+		}
 	}
-	//while(fgets(buffer, sizeof(buffer), f)) {
-	//	printf("%s", buffer);
-	//}
-	
-	pclose(f);
 }
