1b460f2c201312b646517c0197f897ab76de1c96
Daniel Tan <dtan004@hammer.cs.ucr.edu>
2017-11-17 20:34:46 -0800

    Changed tests...

--- tests/multi_command.sh
+++ tests/multi_command.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "invalid command || ifconfig; echo hello world!!!!; git --help; ls -a; git status;" | ./a.out
-echo "ls -a && ifconfig; invalid command || git --help; ls -a || ls && exit;" | ./a.out
-echo "ls || invalid command" | ./a.out
-echo "echo the quick brown fox... && echo jumped over the lazy dog" | ./a.out
-echo "git status && echo Test Successful" | ./a.out
+echo "invalid command || ifconfig; echo hello world!!!!; git --help; ls -a; git status;" | bin/rshell
+echo "ls -a && ifconfig; invalid command || git --help; ls -a || ls && exit;" | bin/rshell
+echo "ls || invalid command" | bin/rshell
+echo "echo the quick brown fox... && echo jumped over the lazy dog" | bin/rshell
+echo "git status && echo Test Successful" | bin/rshell
--- tests/single_command.sh
+++ tests/single_command.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-echo "ls -a;"|./a.out
-echo "git status;"|./a.out
-echo "echo hello world!!!!;"|./a.out
-echo "ls"|./a.out
-echo "ifconfig"|./a.out
-echo "git"|./a.out
+echo "ls -a;"|bin/rshell
+echo "git status;"|bin/rshell
+echo "echo hello world!!!!;"|bin/rshell
+echo "ls"|bin/rshell
+echo "ifconfig"|bin/rshell
+echo "git"|bin/rshell
